7b3066b496454b4f47b2328a25c3b2b3881d87bb
SebastianAMo <sebastian.agudelo2@utp.edu.co>
2023-09-08 18:42:44 -0500

    Error
    Error en la linea 46 con json.loads(data)	|

--- main.py
+++ main.py
@@ -1,72 +1,81 @@
-import asyncio
+import socket
 import json
-from client import send_message
-from models.message import Message, message_from_json
-from models.contact import Contact
+import threading
 
-contacts = []
-user_label = ''
+class Peer:
+    def __init__(self, username, host='127.0.0.1', port=12345):
+        self.username = username
+        self.host = host
+        self.port = port
+        self.contacts = {}  # {username: (ip, port)}
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def load_contacts():
-    global contacts
-    try:
-        with open('data.json', 'r') as f:
-            contacts_data = json.load(f)
-            for contact_data in contacts_data:
-                contact = Contact(contact_data['ip'], contact_data['port'], contact_data['label'])
-                contacts.append(contact)
-    except FileNotFoundError:
-        pass
-
-def save_contacts():
-    contacts_data = [{'ip': contact.ip, 'port': contact.port, 'label': contact.label} for contact in contacts]
-    with open('data.json', 'w') as f:
-        json.dump(contacts_data, f)
-
-async def handle_client(reader, writer):
-    data = await reader.read(100)
-    message = message_from_json(data.decode())
-    print(f"Received {message.content} from {message.sender}")
-
-async def start_server(port, callback):
-    server = await asyncio.start_server(callback, '0.0.0.0', port)
-    async with server:
-        await server.serve_forever()
-
-async def main():
-    global user_label
-    load_contacts()
-    port = int(input("Enter your port: "))
-    user_label = input("Enter your label or username: ")
-    asyncio.create_task(start_server(port, handle_client))
-
-    try:
+    def user_interface(self):
         while True:
-            print("1. Send Message")
-            print("2. Add Contact")
-            print("3. List Contacts")
-            choice = input("Choose an option: ")
-
+            print("1. Conectar con otro Peer")
+            print("2. Enviar mensaje")
+            print("3. Mostrar contactos")
+            print("4. Salir")
+            choice = input("Elige una opción: ")
             if choice == "1":
-                idx = int(input("Enter contact index: "))
-                contact = contacts[idx]
-                msg_content = input("Enter your message: ")
-                message = Message(f'localhost:{port}', msg_content)
-                asyncio.create_task(send_message(contact.ip, contact.port, message, user_label, port))
-
+                host = '127.0.0.1'
+                port = int(input("Introduce el puerto del Peer: "))
+                self.connect_to_peer(host, port)
             elif choice == "2":
-                ip = input("Enter contact IP: ")
-                contact_port = int(input("Enter contact port: "))
-                label = input("Enter contact label: ")
-                contacts.append(Contact(ip, contact_port, label))
-                save_contacts()
-
+                peer_name = input("Introduce el nombre del Peer a quien enviar el mensaje: ")
+                message = input("Escribe tu mensaje: ")
+                if peer_name in self.contacts:
+                    self.send_message(message, *self.contacts[peer_name])
+                else:
+                    print("Peer no encontrado.")
             elif choice == "3":
-                for idx, contact in enumerate(contacts):
-                    print(f"{idx}. {contact.label} ({contact.ip}:{contact.port})")
+                self.show_contacts()
+            elif choice == "4":
+                break
 
-    except KeyboardInterrupt:
-        print("\nExiting chat...")
+    def start_server(self):
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        print(f"Escuchando en {self.host}:{self.port}")
+        while True:
+            client_socket, client_address = self.server_socket.accept()
+            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+
+    def handle_client(self, client_socket):
+        data = client_socket.recv(1024).decode()
+        message_data = dict(data)
+        from_username = message_data.get("from")
+        message = message_data.get("message")
+        print(f"\nNuevo mensaje de {from_username}: {message}\n")
+        if from_username not in self.contacts:
+            self.contacts[from_username] = client_socket.getpeername()
+
+    def connect_to_peer(self, host, port):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((host, port))
+        self.contacts[host] = (host, port)
+        self.send_message(f"Conectado con {self.username}", host, port)
+        
+        print(f"Conectado con {host}:{port}")
+
+    def send_message(self, message, host, port):
+        message_data = {
+            "from": self.username,
+            "message": message
+        }
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((host, port))
+        client_socket.send(json.dumps(message_data).encode())
+        client_socket.close()
+
+    def show_contacts(self):
+        for username, (ip, port) in self.contacts.items():
+            print(f"{username}: {ip}:{port}")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    username = input("Introduce tu nombre de usuario: ")
+    port = int(input("Introduce puerto: "))
+    p = Peer(username,port=port)
+    server_thread = threading.Thread(target=p.start_server)
+    server_thread.start()
+    p.user_interface()
--- models/contact.py
+++
@@ -1,5 +0,0 @@
-class Contact:
-    def __init__(self, ip, port, label):
-        self.ip = ip
-        self.port = port
-        self.label = label
