16a9645ac423fefa604e86b72041d98105ffd5f5
SebastianAMo <sebastian.agudelo2@utp.edu.co>
2023-09-10 10:53:40 -0500

    add message register

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Peer:
         self.port = port
         self.contacts = {}  # {username: (ip, port)}
         self.connected_sockets = {}  # {username: socket}
+        self.messages ={} # {username: [{"from": sender, "message": message}, ...]}
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def user_interface(self):
@@ -16,7 +17,8 @@ class Peer:
             print("1. Conectar con otro Peer")
             print("2. Enviar mensaje")
             print("3. Mostrar contactos")
-            print("4. Salir")
+            print("4. Ver mensajes")
+            print("5. Salir")
             choice = input("Elige una opción: ")
             if choice == "1":
                 host = '127.0.0.1'
@@ -29,11 +31,22 @@ class Peer:
             elif choice == "3":
                 self.show_contacts()
             elif choice == "4":
+                self.view_messages()
+            elif choice == "5":
                 # Close all sockets before exiting
                 for sock in self.connected_sockets.values():
                     sock.close()
                 self.server_socket.close()
                 break
+
+    def view_messages(self):
+        peer_name = input("Introduce el nombre del Peer cuyos mensajes quieres ver: ")
+        if peer_name in self.messages:
+            print(f"Mensajes con {peer_name}:")
+            for msg in self.messages[peer_name]:
+                print(f"{msg['from']}: {msg['message']}")
+        else:
+            print("No hay mensajes con ese Peer.")
 
     def start_server(self):
         self.server_socket.bind((self.host, self.port))
@@ -50,8 +63,12 @@ class Peer:
                 break
             message_data = json.loads(data)
             from_username = message_data.get("from")
-            message = message_data.get("message")
-            print(f"Nuevo mensaje de {from_username}: {message}")
+            
+            # Storing the received message in the 'messages' dictionary
+            if from_username not in self.messages:
+                self.messages[from_username] = []
+            self.messages[from_username].append({"from": from_username, "message": message_data['message']})
+
             if from_username not in self.contacts:
                 self.contacts[from_username] = client_socket.getpeername()
                 port = message_data.get("port")
@@ -66,7 +83,6 @@ class Peer:
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             client_socket.connect((host, port))
             self.send_initial_message(client_socket)
-            # Start a dedicated thread to listen to messages from this peer
             threading.Thread(target=self.handle_client, args=(client_socket,)).start()
             print(f"Conectado con {host}:{port}")
 
@@ -86,6 +102,11 @@ class Peer:
                 "message": message
             }
             self.connected_sockets[peer_name].send(json.dumps(message_data).encode())
+            
+            # Storing the sent message in the 'messages' dictionary
+            if peer_name not in self.messages:
+                self.messages[peer_name] = []
+            self.messages[peer_name].append({"from": self.username, "message": message})
         else:
             print("Peer no encontrado.")
 
@@ -100,5 +121,3 @@ if __name__ == "__main__":
     server_thread = threading.Thread(target=p.start_server)
     server_thread.start()
     p.user_interface()
-    
-
